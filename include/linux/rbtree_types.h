b724a7b2c1db55f0c4ae1a4253b2cfd390ea986e
ByJuanDiego <juan.castro.p@utec.edu.pe>
2025-04-08 11:11:05 -0500

    fix: add 'rb_num_nodes' initialization

--- include/linux/rbtree_types.h
+++ include/linux/rbtree_types.h
@@ -29,7 +29,7 @@ struct rb_root_cached {
 	struct rb_node *rb_leftmost;
 };
 
-#define RB_ROOT (struct rb_root) { NULL, }
-#define RB_ROOT_CACHED (struct rb_root_cached) { {NULL, }, NULL }
+#define RB_ROOT (struct rb_root) { NULL, 0, }
+#define RB_ROOT_CACHED (struct rb_root_cached) { {NULL, 0, }, NULL }
 
 #endif
