bc1b56a298ef0f8262af223d8e4fce4ac8602926
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2025-05-21 13:40:37 -0500

    fix(sched/core): Properly calculate _normal_ priorities
    Probably right (?).

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4345,10 +4345,10 @@ int sched_fork(unsigned long clone_flags, struct task_struct *p)
      * real-time round-robin at the bottom of the RT band.
      */
     p->policy       = SCHED_RR;
-    p->static_prio  = MAX_RT_PRIO - 1;  /* 99 */
-    p->prio         =
-    p->normal_prio  =
-    p->rt_priority  = p->static_prio;
+    p->rt_priority  = 0;
+    p->prio         = normal_prio(p);
+    p->static_prio  = p->prio;
+    p->normal_prio  = p->prio;
 
     uclamp_fork(p);
 
